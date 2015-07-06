f94430a1c781eb94f993d0f2c42f5eead54ca573
Josh.Office <josh@office.com>
2015-07-06 16:57:56 +0800

    library requests intro
    library requests intro

---
+++ 3. RESTful/2. RESTful tools/app.py
@@ -0,0 +1,12 @@
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/index')
+def index():
+    print request.headers
+    return 'Hello'
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ 3. RESTful/2. RESTful tools/request_t.py
@@ -0,0 +1,4 @@
+import requests
+
+headers = {'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2421.0 Safari/537.36"}
+r = requests.get('http://127.0.0.1:5000/index', headers=headers, timeout=1)
