0fc6aa3c3451b9904785d6f6b63646d954df2a94
Josh.Office <josh@office.com>
2015-07-06 17:49:23 +0800

    login validate

--- 3. RESTful/2. RESTful tools/app.py
+++ 3. RESTful/2. RESTful tools/app.py
@@ -3,10 +3,35 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+users = {
+    "josh": ["123456"]
+}
+
+def get_token(uid):
+    token = base64.b64encode(':'.join([
+        str(uid),
+        random.random(),
+        time.time() + 7200
+    ]))
+    users[uid].append(token)
+    return token
+
+def verify_token():
+    pass
+
 @app.route('/index')
 def index():
     print request.headers
     return 'Hello'
+
+@app.route('/login')
+def login():
+    pass
+
+@app.route('/test')
+def test():
+    pass
+
 
 if __name__ == '__main__':
     app.run(debug=True)
