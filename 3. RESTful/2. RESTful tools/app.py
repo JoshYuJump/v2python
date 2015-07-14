4c8461c273b78b207dafee26051da85d52b76c82
josh <josh@ubuntu.pc>
2015-07-15 00:15:49 +0800

    RESTful tools

--- 3. RESTful/2. RESTful tools/app.py
+++ 3. RESTful/2. RESTful tools/app.py
@@ -1,3 +1,6 @@
+import base64
+import random
+import time
 
 from flask import Flask, request
 
@@ -16,21 +19,36 @@ def get_token(uid):
     users[uid].append(token)
     return token
 
-def verify_token():
-    pass
+def verify_token(token):
+    _token = base64.b64decode(token)
+    if not users.get(_token.split(':')[0])[-1] == token:
+        return 1
+    if float(_token.split(':')[-1]) >= time.time():
+        return 1
+    else:
+        return 0
 
-@app.route('/index')
+@app.route('/index', methods=['POST', 'GET'])
 def index():
     print request.headers
     return 'Hello'
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    pass
+    print request.headers
+    uid, pw = base64.b64decode(request.headers['Authorization'].split(':')[-1]).split(':')
+    if users.get(uid)[0] == pw:
+        return get_token(uid)
+    else:
+        return 'error'
 
-@app.route('/test')
+@app.route('/test', methods=['GET', 'POST'])
 def test():
-    pass
+    token = request.args.get('token')
+    if verify_token(token) == 1:
+        return 'data'
+    else:
+        return 'error'
 
 
 if __name__ == '__main__':
--- 3. RESTful/2. RESTful tools/request_t.py
+++ 3. RESTful/2. RESTful tools/request_t.py
@@ -1,4 +1,8 @@
 import requests
 
-headers = {'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2421.0 Safari/537.36"}
-r = requests.get('http://127.0.0.1:5000/index', headers=headers, timeout=1)
+# headers = {'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2421.0 Safari/537.36"}
+# r = requests.get('http://127.0.0.1:5000/index', headers=headers, timeout=1)
+# print r.text
+
+r = requests.get('http://127.0.0.1:5000/login', auth=('josh', '123456'))
+print r.text
